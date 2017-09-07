f07c6440eedc75f95a15c965388f7acd52437700
lkwshqy <1690234425@qq.com>
2017-09-07 11:44:35 +0800

    OK

---
+++ bjhhcbj/src/main/java/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	/**
+	 * @MethodName main
+	 * @author lkw
+	 * @Description
+	 * @version 
+	 * @Date 上午11:43:45
+	 * @return void
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ok");
+	}
+
+}
